a89ed1b6df9c45ab03a3723e1a96eecf8841e925
Jordi Anguela <jordi@codium.team>
2021-02-23 15:26:03 +0100

    use Prettier to format files
    Co-authored-by: Luis Rovirosa Mairlot <luis@codium.team>

--- js/test/TheHouseThatJackBuild.test.js
+++ js/test/TheHouseThatJackBuild.test.js
@@ -1,5 +1,5 @@
-describe("the-house-that-jack-built", () => {
-  it("should do something", () => {
+describe('the-house-that-jack-built', () => {
+  it('should do something', () => {
     expect(true).toBe(true);
   });
 });
