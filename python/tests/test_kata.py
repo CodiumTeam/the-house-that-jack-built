9965dfc66ea0ccaff07544ddbd99ce083b318d1b
Jordi Anguela <jordi@codium.team>
2025-03-13 12:20:24 +0100

    test_regular_song_generates_first_verse
    Co-authored-by: Celia Sierra <celia.sierra@clarity.ai>

--- python/tests/test_kata.py
+++ python/tests/test_kata.py
@@ -1,7 +1,11 @@
 import unittest
 
+from python.the_house_that_jack_built.TheHouseThatJackBuiltSong import TheHouseThatJackBuiltSong
+
 
 class KataTest(unittest.TestCase):
 
-    def test_xxx(self):
-        self.assertEqual(True, True)
+    def test_regular_song_generates_first_verse(self):
+        verses = TheHouseThatJackBuiltSong().generate_song()
+
+        self.assertEqual("This is the house that Jack built.", verses[0])
---
+++ python/the_house_that_jack_built/TheHouseThatJackBuiltSong.py
@@ -0,0 +1,7 @@
+
+
+class TheHouseThatJackBuiltSong:
+    @classmethod
+    def generate_song(cls):
+        verses = ["This is the house that Jack built."]
+        return verses
